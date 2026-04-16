de5991a9d2fbc70776fb953455b2c8bf83c25c87
Izzatilla <izzatillasultanov@gmail.com>
2026-04-16 20:43:46 +0500

    Add controller init

--- src/main/java/com/example/demo_git/controller/UserController.java
+++ src/main/java/com/example/demo_git/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.example.demo_git.controller;
 
+import com.example.demo_git.dto.UserCreateDTO;
+import com.example.demo_git.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/save")
+    public void save(@RequestBody UserCreateDTO userCreateDTO) {
+        userService.save(userCreateDTO);
+
+    }
 }
